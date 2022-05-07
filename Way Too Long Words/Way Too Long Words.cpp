c28ab2920eac08d83b653ad1c80b17d479979ea7
Mohamed Shaalan <78407122+mohamedwshaalan@users.noreply.github.com>
2022-05-07 18:28:46 +0200

    Update Way Too Long Words.cpp

--- Way Too Long Words/Way Too Long Words.cpp
+++ Way Too Long Words/Way Too Long Words.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
-
+#include <fstream>
+#include <istream>
+#include <iostream>
+#include <string>
+using namespace std;
 int main()
 {
-    std::cout << "testing" << std::endl;
-}
 
+    int number;
+    string word;
+    cin >> number;
+
+    for (int i = 0; i < number; i++) {
+        cin >> word;
+        if (word.size() > 10) {
+            cout << word[0] << (word.size() - 2) << word[word.size() - 1];
+            cout << endl;
+        }
+        else {
+            cout << word;
+            cout << endl;
+        }
+    }
+
+}
